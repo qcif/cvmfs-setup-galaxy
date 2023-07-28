86471ce1db26b127d66929ffc4f6a0ad63ec8f41
Hoylen Sue <hoylen@hoylen.com>
2023-07-28 15:59:16 +1000

    Updated supported distribution code to be more futureproof.

--- cvmfs-galaxy-proxy-setup.sh
+++ cvmfs-galaxy-proxy-setup.sh
@@ -21,7 +21,7 @@
 #================================================================
 
 PROGRAM='cvmfs-galaxy-proxy-setup'
-VERSION='1.3.1'
+VERSION='1.3.2'
 
 EXE=$(basename "$0" .sh)
 EXE_EXT=$(basename "$0")
@@ -239,24 +239,25 @@ else
   DISTRO=unknown
 fi
 
-case "$DISTRO" in
-  'CentOS Linux release 7.'* \
-    | 'CentOS Linux release 8.'* \
-    | 'CentOS Stream release 8' \
-    | 'Rocky Linux release 8.8 (Green Obsidian)' \
-    | 'Rocky Linux release 8.5 (Green Obsidian)' \
-    | 'Rocky Linux release 8.6 (Green Obsidian)' \
-    | 'Rocky Linux release 9.0 (Blue Onyx)' \
-    | 'Rocky Linux release 9.2 (Blue Onyx)' \
-    | 'Ubuntu 21.04' \
-    | 'Ubuntu 20.04' \
-    | 'Ubuntu 20.10' )
-    # Tested distribution (add to above, if others have been tested)
-    ;;
-  *)
-    echo "$EXE: warning: untested system: $DISTRO" >&2
-  ;;
-esac
+# Add to patterns below, if other distribututions have been tested.
+
+TESTED=
+for PATTERN in \
+  '^CentOS Linux release 7\..*$' \
+  '^CentOS Linux release 8\..*$' \
+  '^CentOS Stream release 8\..*$' \
+  '^Rocky Linux release 8\..*$' \
+  '^Ubuntu 2[0-9]\..*$' \
+  ;
+do
+  if echo "$DISTRO" | grep -qE "$PATTERN" ; then
+    TESTED=yes
+  fi
+done
+
+if [ -z "$TESTED" ]; then
+  echo "$EXE: warning: untested system: $DISTRO" >&2
+fi
 
 #----------------------------------------------------------------
 # Check for root privileges
